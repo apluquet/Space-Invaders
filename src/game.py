2604b0c37afc4de57b7071a3e40a0c8d7e252dde
Adèle Pluquet <adele.pluquet@epita.fr>
2023-07-27 14:12:04 +0000

    Refactor add invaders class

--- src/game.py
+++ src/game.py
@@ -1,31 +1,15 @@
 import pygame
 import numpy as np
-import random
 
 from bullet import Bullet
 from player import Player
+from invaders import Invaders
+
 
 # This class represents the game of Space Invaders. It manages the position of
 # each element and saves all object of the game.
 class Game:
     # Private class variables
-
-    # Step size in pixels for invaders movement
-    __step_x_size = 5
-    __step_y_size = 30
-
-    # Image of an invader
-    __invader_img = pygame.image.load("img/my_invader.png")
-
-    __margin_x_btw_invaders = 25
-    __margin_y_btw_invaders = 10
-
-    # Size in pxl of the invader image
-    __invader_width = __invader_img.get_size()[0]
-    __invader_height = __invader_img.get_size()[1]
-
-    __nb_invaders_cols = 11
-    __nb_invaders_lines = 5
 
     # Margin between borders of the window and the game in pixels
     __margin = 20
@@ -36,27 +20,14 @@ class Game:
         self.__width = width
         self.__height = height
 
-        # Numpy array saving all invaders
-        self.__invaders = np.ones(
-            (self.__nb_invaders_lines, self.__nb_invaders_cols), dtype=int
-        )
-
-        # Size in pixels of a screen subdivision to place one invader
-        # self.__invader_square_size = self.__width / self.__nb_squares_width
-
         # Time in ms between invaders move
         self.__move_time = 100
         # Last time a move was made
         self.__last_move_time = 0
 
-        # For the invaders movement, save the last (x,y) position of the top
-        # right invader
-        self.__invaders_last_x = self.__margin
-        self.__invaders_last_y = self.__margin
-
-        # For the invaders movement, save if invaders movement is goind right
-        # or not
-        self.__move_right = True
+        self.__invaders = Invaders(
+            self.__margin, self.__margin, self.__margin, self.__height - 150
+        )
 
         self.__player = Player(
             self.__width / 2, self.__height - 150, self.__width, self.__margin
@@ -66,66 +37,24 @@ class Game:
         self.__bottom_screen_game = self.__height - 100
 
     # Private class methods
-    def __is_at_right_border(self):
-        # Check if at right border
-
-        # x_pos + nb_cols * (invader_width + margin_btw_invaders)
-        # - margin_btw_invaders + width_invader
-        # Remove one margin for the invader furthest to the right
-
-        if (
-            self.__invaders_last_x
-            + self.__nb_invaders_cols
-            * (self.__invader_width + self.__margin_x_btw_invaders)
-            - self.__margin_x_btw_invaders
-            + self.__step_x_size
-            >= self.__width - self.__margin
-        ):
-            return True
-        return False
-
-    def __is_at_left_border(self):
-        # Check if at left border
-
-        if self.__invaders_last_x - self.__step_x_size < self.__margin:
-            return True
-        return False
-
-    def __can_move_side(self):
-        # Check if it can move side according to the direction and position of
-        # the borders
-        return (self.__move_right and not self.__is_at_right_border()) or (
-            not self.__move_right and not self.__is_at_left_border()
-        )
 
     def __display_invaders(self, screen, time):
-        move = False
-
         # Set move to True if enough elapsed time since last movement
         if time - self.__last_move_time > self.__move_time:
             self.__last_move_time += self.__move_time
-            move = True
-
-        if move and self.__can_move_side():
-            self.__invaders_last_x += (
-                self.__step_x_size if self.__move_right else -self.__step_x_size
-            )
-
-        elif move and not self.__can_move_side():
-            self.__invaders_last_y += self.__step_y_size
-            self.__move_right = not self.__move_right
+            self.__invaders.move(self.__width)
 
         # Display the invaders with movement
-        for i in range(self.__nb_invaders_cols):
-            for j in range(self.__nb_invaders_lines):
-                if self.__invaders[j, i]:
-                    x = self.__invaders_last_x + i * (
-                        self.__invader_width + self.__margin_x_btw_invaders
+        for i in range(self.__invaders.nb_cols):
+            for j in range(self.__invaders.nb_lines):
+                if self.__invaders.invaders[j, i]:
+                    x = self.__invaders.x + i * (
+                        self.__invaders.width + self.__invaders.margin_x
                     )
-                    y = self.__invaders_last_y + j * (
-                        self.__invader_height + self.__margin_y_btw_invaders
+                    y = self.__invaders.y + j * (
+                        self.__invaders.height + self.__invaders.margin_y
                     )
-                    screen.blit(self.__invader_img, (x, y))
+                    screen.blit(self.__invaders.img, (x, y))
 
     def __display_player(self, screen):
         self.__player.get_keyboard_input()
@@ -158,7 +87,7 @@ class Game:
         # Display remaining invaders
         text_font = pygame.font.Font("fonts/moder_dos_437.ttf", 20)
         text_remaining_invaders = text_font.render(
-            "Remaining invaders " + str(self.__invaders.sum()), False, "white"
+            "Remaining invaders " + str(self.__invaders.remaining()), False, "white"
         )
         screen.blit(text_remaining_invaders, (self.__width - 350, self.__height - 60))
 
@@ -215,7 +144,7 @@ class Game:
             text_end = text_win_font.render("Defeat", False, "red")
 
         text_score = text_score_font.render(
-            "Remaining invaders " + str(self.__invaders.sum()), False, "white"
+            "Remaining invaders " + str(self.__invaders.remaining()), False, "white"
         )
 
         # Get width of each text in order to center them
@@ -232,45 +161,17 @@ class Game:
             text_score, (self.__width // 2 - w_text_score // 2, y_top_left + 230)
         )
 
-    def __get_invader_pos(self, line, col):
-        x = self.__invaders_last_x + col * (
-            self.__invader_width + self.__margin_x_btw_invaders
-        )
-        y = self.__invaders_last_y + line * (
-            self.__invader_height + self.__margin_y_btw_invaders
-        )
-        return (x, y)
-
     def __invaders_shoot(self, screen):
-        probability_shoot = 0.01
-        for i in range(self.__nb_invaders_cols):
-            j = self.__nb_invaders_lines - 1
-            while not self.__invaders[j][i] and j >= 0:
-                j -= 1
-            if j >= 0:
-                nb = random.random()
-                if nb <= probability_shoot:
-                    self.__invaders[j][i] = 0
-
-                    x, y = self.__get_invader_pos(j, i)
-                    x += self.__invader_width // 2
-                    y += self.__invader_height
-                    new_bullet = Bullet(x, y, (0, 1), True)
-                    self.__bullets.append(new_bullet)
+        self.__bullets += self.__invaders.shoot()
 
     def __check_victory(self, screen):
         # If no more invaders, player win
-        if self.__invaders.sum() == 0:
+        if self.__invaders.remaining() == 0:
             self.__endgame(screen, win=True)
             return False
 
         # If invaders are too low (same level as the player), player loses
-        if (
-            self.__invaders_last_y
-            + self.__nb_invaders_lines
-            * (self.__invader_height + self.__margin_y_btw_invaders)
-            > self.__height - 150
-        ):
+        if self.__invaders.too_low():
             self.__endgame(screen, win=False)
             return False
 
---
+++ src/invaders.py
@@ -0,0 +1,98 @@
+import random
+import pygame
+import numpy as np
+
+from bullet import Bullet
+
+class Invaders:
+    img = pygame.image.load("img/my_invader.png")
+    width = img.get_size()[0]
+    height = img.get_size()[1]
+
+    margin_x = 25
+    margin_y = 10
+
+    step_x = 5
+    step_y = 30
+
+    nb_cols = 11
+    nb_lines = 5
+
+    shoot_probability = 0.01
+
+    def __init__(self, x, y, board_margin, bottom_limit):
+        self.invaders = np.ones((self.nb_lines, self.nb_cols), int)
+
+        # Position (in pixel) of the top left invader of the group
+        self.x = x
+        self.y = y
+
+        self.board_margin = board_margin
+        self.bottom_limit = bottom_limit
+
+        self.move_right = True
+
+    def remaining(self):
+        return self.invaders.sum()
+
+    def too_low(self):
+        y_lowest_invader = self.y + self.nb_lines * (self.height + self.margin_y)
+        return y_lowest_invader > self.bottom_limit
+
+    def is_at_right_border(self, board_width):
+        max_right = board_width - self.board_margin
+
+        # Get the position the most at right of the group if moves to right
+        # If higher than the right limit of the board (max_right), then, is
+        # at right border
+        right_invaders = self.x + self.nb_cols * (self.width + self.margin_x)
+        right_invaders += self.step_x - self.margin_x
+
+        return right_invaders >= max_right
+
+    def is_at_left_border(self):
+        # Check if at left border
+        return self.x - self.step_x < self.board_margin
+
+    def can_move_side(self, board_width):
+        # Check if it can move side according to the direction and position of
+        # the borders
+        return (self.move_right and not self.is_at_right_border(board_width)) or (
+            not self.move_right and not self.is_at_left_border()
+        )
+
+    def move(self, board_width):
+        if self.can_move_side(board_width):
+            self.x += self.step_x if self.move_right else -self.step_x
+        else:
+            self.y += self.step_y
+            self.move_right = not self.move_right
+
+    def get_invader_pos(self, line, col):
+        x = self.x + col * (self.width + self.margin_x)
+        y = self.y + line * (self.height + self.margin_y)
+        return (x, y)
+
+    def shoot(self):
+        # Lit of new bullets 
+        bullets = []
+
+        # Find for each column, the invader at the lowest spot
+        for i in range(self.nb_cols):
+            j = self.nb_lines - 1
+
+            while not self.invaders[j][i] and j >= 0:
+                j -= 1
+
+            if j >= 0:
+                nb = random.random()
+                if nb <= self.shoot_probability:
+                    self.invaders[j][i] = 0
+
+                    x, y = self.get_invader_pos(j, i)
+                    x += self.width // 2
+                    y += self.height
+                    new_bullet = Bullet(x, y, (0, 1), True)
+                    bullets.append(new_bullet)
+
+        return bullets
